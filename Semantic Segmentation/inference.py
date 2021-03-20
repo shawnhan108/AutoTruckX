bea43a84bd221024e90a70861ea73905a6b6ad06
Shawn (Yushuo) Han <shawnhan1999@hotmail.com>
2021-03-20 13:14:07 -0400

    Improved inference code. More training instances for UNet.

--- Semantic Segmentation/inference.py
+++ Semantic Segmentation/inference.py
@@ -8,12 +8,17 @@ from PIL import Image
 import cv2
 import matplotlib.pyplot as plt
 
-from utils import select_model, load_weights, preprocess_img
+from utils import select_model, load_weights, preprocess_img, get_clustering_model, split_img
 from config import best_ckpt_src, net, inf_img_src, inf_out_img_src, IMG_DIM, device, \
                     inf_vid_src, inf_out_vid_src
 
-def inference_image(model, logger, img=np.array(Image.open(inf_img_src)), record=True, dpi=500):
+def inference_image(model, logger, img=np.array(Image.open(inf_img_src).convert('RGB')), compare=True, record=True, dpi=500):
 
+    if compare:
+        assert img.shape[1] == IMG_DIM * 2
+        img, mask = split_img(img, IMG_DIM)
+
+    orig_img = img.copy()
     img = preprocess_img(img)
     img = img.to(device)
 
@@ -29,17 +34,36 @@ def inference_image(model, logger, img=np.array(Image.open(inf_img_src)), record
     plt.savefig(buf, format='jpg', dpi=dpi)
     buf.seek(0)
 
-    y_pred = Image.open(buf).resize((IMG_DIM, IMG_DIM), Image.LANCZOS).convert("RGB")
-    y_pred = cv2.cvtColor(np.array(y_pred), cv2.COLOR_RGB2BGR)
+    y_pred_out = Image.open(buf).resize((IMG_DIM, IMG_DIM), Image.LANCZOS).convert("RGB")
+    y_pred_out = cv2.cvtColor(np.array(y_pred_out), cv2.COLOR_RGB2BGR)
 
-    cv2.imshow("Output", y_pred)
+    # compare 
+    if compare:
+
+        # Get GT
+        cluster_model = get_clustering_model(logger)
+        mask = cv2.resize(mask, (IMG_DIM, IMG_DIM), interpolation=cv2.INTER_AREA)
+        class_map = cluster_model.predict(mask.reshape(-1, 3)).reshape(IMG_DIM, IMG_DIM)
+        
+        # IoU
+        intersection = np.logical_and(class_map, y_pred)
+        union = np.logical_or(class_map, y_pred)
+        iou_score = np.sum(intersection) / np.sum(union)
+
+        # Visualize
+        class_map_out = cv2.putText(mask, 'GT, IoU: {0}'.format(round(iou_score, 3)), (20, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2, cv2.LINE_AA) 
+        y_pred_out = cv2.putText(y_pred_out, 'Prediction', (20, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2, cv2.LINE_AA) 
+        orig_img = cv2.putText(orig_img, 'Image', (20, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2, cv2.LINE_AA)
+        y_pred_out = np.concatenate((orig_img, y_pred_out, class_map_out), axis=1)
 
     # Record
     if record:
-        cv2.imwrite(inf_out_img_src, y_pred)
+        cv2.imwrite(inf_out_img_src, y_pred_out)
         logger.info("(3) Inference Finished. Output image: {0}".format(inf_out_img_src))
     
-    return y_pred
+    cv2.imshow("Output", y_pred_out)
+
+    return y_pred_out
 
 def inference_video(model, logger, record=True, dpi=500):
 
@@ -58,7 +82,7 @@ def inference_video(model, logger, record=True, dpi=500):
         ret, frame = video_source.read()
         frame_show = frame.copy()
 
-        frame_show = inference_image(model, logger, img=frame_show, record=False, dpi=dpi)
+        frame_show = inference_image(model, logger, img=frame_show, compare=False, record=False, dpi=dpi)
         
         if record:
             video_out.write(frame_show)
@@ -81,7 +105,8 @@ if __name__ == "__main__":
 
     # load model weights
     load_weights(model, best_ckpt_src, logger)
+    model = model.to(device)
 
     # inference 
-    inference_image(model, logger)
+    inference_image(model, logger, compare=True)
     # inference_video(model, logger)
